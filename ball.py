f59e132bc05a5f118b16ad4f98fd6671eb43fa5c
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-06 16:13:42 -0700

    first commit

---
+++ ball.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Ball(pygame.Rect):
+    def __init__(self, velocity, *args, **kwargs):
+        self.velocity = velocity
+        self.angle = 0
+        super().__init__(*args, **kwargs)
+
+    def move_ball(self):
+        self.x += self.velocity
+        self.y += self.angle
