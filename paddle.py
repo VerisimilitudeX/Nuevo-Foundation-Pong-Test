6970c62cdcd0e54dd751e3a2c56f7c348fa012d6
Piyush Acharya <acharyapiyush1@outlook.com>
2022-08-08 14:04:29 -0700

    Code refactoring

---
+++ paddle.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Paddle(pygame.Rect):
+    def __init__(self, velocity, up_key, down_key, *args, **kwargs):
+        self.velocity = velocity
+        self.up_key = up_key
+        self.down_key = down_key
+        super().__init__(*args, **kwargs)
+
+    def move_paddle(self, board_height):
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[self.up_key]:
+            if self.y - self.velocity > 0:
+                self.y -= self.velocity
+
+        if keys_pressed[self.down_key]:
+            if self.y + self.velocity < board_height - self.height:
+                self.y += self.velocity
